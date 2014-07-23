d5e42970516d3de34d932f50a7822fee836eaf1c
Administrator <administrator@Mac-Administrator.local>
2014-07-23 06:50:33 -0700

    Initial Commit

---
+++ Office/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Office
+//
+//  Created by Administrator on 7/23/14.
+//  Copyright (c) 2014 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
